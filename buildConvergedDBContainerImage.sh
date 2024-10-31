8ad80811a422514f4eadb6fae5ffc86e9929d039
firuzcetinkaya <49010638+firuzcetinkaya@users.noreply.github.com>
2024-10-31 14:26:17 +0300

    Update buildConvergedDBContainerImage.sh

--- buildConvergedDBContainerImage.sh
+++ buildConvergedDBContainerImage.sh
@@ -11,10 +11,13 @@ DOCKER_NETWORK_NAME="ora_net"
 NETWORK_SUBNET="172.11.0.0/24"
 NETWORK_IP_RANGE="172.11.0.0/24"
 NETWORK_GATEWAY="172.11.0.1"
-DB_HOST_NAME=
-DB_IP=
+DB_HOST_NAME="CONVERGED_DB_FREE"
+DB_IP="172.11.0.2"
 DB_PORT=1521
-ORACLE_PWD=Oracle123
+ORACLE_PASSWD="Oracle123"
+DB_EXPOSE_PORT=1525
+
+
 
 echo "Checking Docker installation"
 if command -v docker &> /dev/null; then
@@ -34,11 +37,16 @@ echo "Pulling ORDS-Developer Image from Oracle Container Registry"
 docker pull container-registry.oracle.com/database/ords-developer:latest
 
 
-echo "Creating Docker Network ora_net"
-docker network create --driver=bridge --subnet=172.11.0.0/24 --ip-range=172.11.0.0/24 --gateway=172.11.0.1 ora_net
-if [ $(docker network ls|grep ora_net|wc -l) -gt 0 ]; then
-    docker network ls|grep ora_net
+echo "Creating Docker Network ""${DOCKER_NETWORK_NAME}"""
+docker network create --driver=bridge --subnet=${NETWORK_SUBNET} --ip-range=${NETWORK_IP_RANGE} --gateway=${NETWORK_GATEWAY} ${DOCKER_NETWORK_NAME}
+if [ $(docker network ls|grep ${DOCKER_NETWORK_NAME}|wc -l) -gt 0 ]; then
+    docker network ls|grep ${DOCKER_NETWORK_NAME}
 else
-    echo "Docker network ora_net could'nt created. Please tyr again."
+    echo "Docker network "${DOCKER_NETWORK_NAME}" couldn't created. Please tyr again."
     exit 1
 fi
+
+
+echo "Running Oracle 23ai Free Docker Image with configured parameters"
+docker run -td --name ${DB_HOST_NAME} --hostname ${DB_HOST_NAME} --network ${DOCKER_NETWORK_NAME} --ip ${DB_IP} -p ${DB_EXPOSE_PORT}:${DB_PORT} -e ORACLE_PWD=${ORACLE_PASSWD} container-registry.oracle.com/database/free:latest
+
